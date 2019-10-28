b64c02577ea942fc359bd8cdf20ea6e13bf21e07
RobbeBrandse <robbe.brandse@student.howest.be>
2019-10-28 19:47:43 +0100

    init map
    Create the map and make it view the current location of the user or the default location: Kortrijk

--- script/app.js
+++ script/app.js
@@ -1,18 +1,37 @@
-function handleFloatingLabel() {
-    let input = document.querySelector('.js-floating-input'),
-        label = document.querySelector('.js-floating-label');
+'use strict';
 
-    input.addEventListener('blur', function () {
-        console.log(event);
-        if (input.value) {
-            label.classList.add('is-floating');
-        } else {
-            label.classList.remove('is-floating');
-        }
-    });
-}
+let mymap;
+let position;
+var map;
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-});
+const initmap = function() {
+	console.log('init map');
+	console.log(position);
+	if (position != null) {
+		map = L.map('mapid').setView(position, 13);
+		L.tileLayer.provider('Wikimedia').addTo(map);
+	}
+};
+
+const get_location = function() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			console.log(pos);
+			position = [pos.coords.latitude, pos.coords.longitude];
+			console.log('position: ' + position);
+			initmap();
+		});
+	} else {
+		postition = [55.824894, 3.249808];
+		console.log('Geolocation is not supported by this browser, defaulting to Kortijk');
+		initmap();
+	}
+};
+
+const init = function() {
+	console.log('dom geladen');
+	// console.log(get_location());
+	get_location();
+	//initmap();
+};
+document.addEventListener('DOMContentLoaded', init);
