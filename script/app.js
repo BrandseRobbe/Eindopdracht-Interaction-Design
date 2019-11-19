d5904f131b0bcde94b790860af74b84d13237e54
= <robbe.brandse@student.howest.be>
2019-11-19 16:39:39 +0100

    verdere opmaak filter
    toevoeging css en html

--- script/app.js
+++ script/app.js
@@ -12,65 +12,65 @@ var options;
 
 let satellite_icon;
 const delete_watermark = function() {
-	//console.log('deleting watermark'); this function deletes the watermark in the corner
-	let watermark = document.querySelector('.cesium-credit-textContainer');
-	watermark.parentNode.removeChild(watermark);
+  //console.log('deleting watermark'); this function deletes the watermark in the corner
+  let watermark = document.querySelector('.cesium-credit-textContainer');
+  watermark.parentNode.removeChild(watermark);
 };
 
 const show_satellite = async function() {
-	//get satellite date from API and puts them into an object
-	let endpoint = `${url}/${position[0]}/${position[1]}/0/10/0/${key}`;
-	console.log('getting: ' + endpoint);
-	const get = await fetch(endpoint);
-	const satellites = await get.json();
+  //get satellite date from API and puts them into an object
+  let endpoint = `${url}/${position[0]}/${position[1]}/0/10/0/${key}`;
+  console.log('getting: ' + endpoint);
+  const get = await fetch(endpoint);
+  const satellites = await get.json();
 
-	//console.log(satellites);
-	for (let satellite of satellites.above) {
-		//console.log(satellite);
-		var marker = WE.marker([satellite.satlat, satellite.satlng], './img/svg/satelliet.svg', 80, 80).addTo(earth);
-		marker.bindPopup(satellite.satname, { maxWidth: 70 });
-	}
+  //console.log(satellites);
+  for (let satellite of satellites.above) {
+    //console.log(satellite);
+    var marker = WE.marker([satellite.satlat, satellite.satlng], './img/svg/satelliet.svg', 80, 80).addTo(earth);
+    marker.bindPopup(satellite.satname, { maxWidth: 70 });
+  }
 };
 
 const initmap = function() {
-	console.log('init map');
-	console.log(position);
-	if (position != null) {
-		options = {
-			atmosphere: true,
-			center: position,
-			zoom: 7,
-			minZoom: 1,
-			maxZoom: 5
-		};
-		earth = new WE.map('js-earth', options);
-		// og: http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg
-		//https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
-		//https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}
-		WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', options).addTo(earth);
+  console.log('init map');
+  console.log(position);
+  if (position != null) {
+    options = {
+      atmosphere: true,
+      center: position,
+      zoom: 7,
+      minZoom: 1,
+      maxZoom: 5
+    };
+    earth = new WE.map('js-earth', options);
+    // og: http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg
+    //https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
+    //https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}
+    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', options).addTo(earth);
 
-		delete_watermark();
-		show_satellite();
-	}
+    delete_watermark();
+    show_satellite();
+  }
 };
 
 const get_location = function() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(pos) {
-			console.log(pos);
-			position = [pos.coords.latitude, pos.coords.longitude];
-			console.log('position: ' + position);
-			initmap();
-		});
-	} else {
-		postition = [55.824894, 3.249808];
-		console.log('Geolocation is not supported by this browser, defaulting to Kortijk');
-		initmap();
-	}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      console.log(pos);
+      position = [pos.coords.latitude, pos.coords.longitude];
+      console.log('position: ' + position);
+      initmap();
+    });
+  } else {
+    postition = [55.824894, 3.249808];
+    console.log('Geolocation is not supported by this browser, defaulting to Kortijk');
+    initmap();
+  }
 };
 
 const init = function() {
-	console.log('dom geladen');
-	get_location();
+  console.log('dom geladen');
+  //get_location();
 };
 document.addEventListener('DOMContentLoaded', init);
