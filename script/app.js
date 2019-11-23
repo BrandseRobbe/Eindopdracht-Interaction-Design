748bd723b25b2951040ed1f5cb552898eb60d035
RobbeBrandse <robbe.brandse@student.howest.be>
2019-11-23 01:18:27 +0100

    rough features added
    list of satelite types shows properly
    when selecting a satellite type
    popup width changes depending of length of satellite name
    
    fix: popup text not showing

--- script/app.js
+++ script/app.js
@@ -7,70 +7,170 @@ var map;
 let url = 'https://www.n2yo.com/rest/v1/satellite/above/';
 let key = '?apiKey=6MTZQ7-QW9KS9-X73GC5-47T0';
 
+let sat_types = [
+	'Brightest',
+	'ISS',
+	'Weather',
+	'NOAA',
+	'GOES',
+	'Earth resources',
+	'Search & rescue',
+	'Disaster monitoring',
+	'Tracking and Data Relay Satellite System',
+	'Geostationary',
+	'Intelsat',
+	'Gorizont',
+	'Raduga',
+	'Molniya',
+	'Iridium',
+	'Orbcomm',
+	'Globalstar',
+	'Amateur radio',
+	'Experimental',
+	'Global Positioning System (GPS) Operational',
+	'Glonass Operational',
+	'Galileo',
+	'Satellite-Based Augmentation System',
+	'Navy Navigation Satellite System',
+	'Russian LEO Navigation',
+	'Space & Earth Science',
+	'Geodetic',
+	'Engineering',
+	'Education',
+	'Military',
+	'Radar Calibration',
+	'CubeSats',
+	'XM and Sirius',
+	'TV',
+	'Beidou Navigation System',
+	'Yaogan',
+	'Westford Needles',
+	'Parus',
+	'Strela',
+	'Gonets',
+	'Tsiklon',
+	'Tsikada',
+	'O3B Networks',
+	'Tselina',
+	'Celestis',
+	'IRNSS',
+	'QZSS',
+	'Flock',
+	'Lemur',
+	'Global Positioning System (GPS) Constellation',
+	'Glonass Constellation',
+	'Starlink'
+];
+let displayed_sats = [];
+
 var earth;
 var options;
 
 let satellite_icon;
 const delete_watermark = function() {
-  //console.log('deleting watermark'); this function deletes the watermark in the corner
-  let watermark = document.querySelector('.cesium-credit-textContainer');
-  watermark.parentNode.removeChild(watermark);
+	//console.log('deleting watermark'); this function deletes the watermark in the corner
+	let watermark = document.querySelector('.cesium-credit-textContainer');
+	watermark.parentNode.removeChild(watermark);
 };
 
-const show_satellite = async function() {
-  //get satellite date from API and puts them into an object
-  let endpoint = `${url}/${position[0]}/${position[1]}/0/10/0/${key}`;
-  console.log('getting: ' + endpoint);
-  const get = await fetch(endpoint);
-  const satellites = await get.json();
-
-  //console.log(satellites);
-  for (let satellite of satellites.above) {
-    //console.log(satellite);
-    var marker = WE.marker([satellite.satlat, satellite.satlng], './img/svg/satelliet.svg', 80, 80).addTo(earth);
-    marker.bindPopup(satellite.satname, { maxWidth: 70 });
-  }
+const show_satellite = async function(typeid) {
+	//get satellite date from API and puts them into an object
+	let endpoint = `${url}/${position[0]}/${position[1]}/0/10/${typeid}/${key}`;
+	console.log('getting: ' + endpoint);
+	const get = await fetch(endpoint);
+	const satellites = await get.json();
+	try {
+		for (let satellite of satellites.above) {
+			// popup width zelf berekenen omdat het een vaste waarde moet krijgen
+			let width = 0;
+			for (const char of satellite.satname) {
+				width += 8;
+			}
+			//satelliet image en popup met de naam weergeven
+			var marker = WE.marker([satellite.satlat, satellite.satlng], './img/svg/satelliet.svg', 80, 80).addTo(earth);
+			marker.bindPopup(satellite.satname, { maxWidth: width });
+		}
+	} catch {
+		console.log('none found');
+	}
 };
 
 const initmap = function() {
-  console.log('init map');
-  console.log(position);
-  if (position != null) {
-    options = {
-      atmosphere: true,
-      center: position,
-      zoom: 7,
-      minZoom: 1,
-      maxZoom: 5
-    };
-    earth = new WE.map('js-earth', options);
-    // og: http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg
-    //https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
-    //https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}
-    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', options).addTo(earth);
+	console.log('init map');
+	console.log(position);
+	if (position != null) {
+		options = {
+			atmosphere: true,
+			center: position,
+			zoom: 7,
+			minZoom: 1,
+			maxZoom: 5
+		};
+		earth = new WE.map('js-earth', options);
+		// og: http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg
+		//https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
+		//https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}
+		WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', options).addTo(earth);
 
-    delete_watermark();
-    show_satellite();
-  }
+		delete_watermark();
+		// show_satellite(1);
+	}
 };
 
 const get_location = function() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      console.log(pos);
-      position = [pos.coords.latitude, pos.coords.longitude];
-      console.log('position: ' + position);
-      initmap();
-    });
-  } else {
-    postition = [55.824894, 3.249808];
-    console.log('Geolocation is not supported by this browser, defaulting to Kortijk');
-    initmap();
-  }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			console.log(pos);
+			position = [pos.coords.latitude, pos.coords.longitude];
+			console.log('position: ' + position);
+			initmap();
+		});
+	} else {
+		postition = [55.824894, 3.249808];
+		console.log('Geolocation is not supported by this browser, defaulting to Kortijk');
+		console.log('position: ' + position);
+		initmap();
+	}
 };
 
+const select_option = function(object) {
+	// console.log(object.id);
+	if (!displayed_sats.includes(object.id)) {
+		displayed_sats.push(object.id);
+	} else {
+		displayed_sats.pop(object.id);
+	}
+	console.log(displayed_sats);
+	for (let satnaam of displayed_sats) {
+		//console.log(satnaam);
+		//console.log(sat_types.indexOf(satnaam));
+		show_satellite(sat_types.indexOf(satnaam) + 1);
+	}
+};
+
+const load_types = function() {
+	let typelist = document.querySelector('#js-typelist');
+	typelist.innerHTML = '';
+	for (let sat_name of sat_types) {
+		let typeid = sat_types.indexOf(sat_name) + 1;
+		// speciaal karacter verwijderen
+		// let idname = sat_name.replace('&', '');
+
+		typelist.innerHTML += `
+      <li class="c-custom-option">
+        <input class="o-hide-accessible c-option--hidden" type="checkbox" id="${sat_name}" value="${typeid}" onclick='select_option(this);' />
+        <label class="c-filter-sat" for="${sat_name}">
+          <p class="c-filter-satnaam">${sat_name}</p>
+          <svg class="c-checkicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64.91 54.66">
+            <polyline points="2 26.2 25.6 52.66 62.91 2" />
+          </svg>
+        </label>
+      </li>`;
+	}
+};
 const init = function() {
-  console.log('dom geladen');
-  //get_location();
+	console.log('dom geladen');
+	get_location();
+	load_types();
 };
 document.addEventListener('DOMContentLoaded', init);
